c79628a11a848001b5c0758deec5f6f8ee23710c
Jared Chapman <thejaredchapman@gmail.com>
2022-03-28 10:38:28 -0500

    Cleaned and transformed
    Cleaned and Transformed files from CSV to JSON file using FastAPI

---
+++ server/main.py
@@ -0,0 +1,21 @@
+
+import os
+import pandas as pd
+from fastapi import FastAPI
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+CACHE_DIR = os.path.join(BASE_DIR, 'cache')
+
+dataset = os.path.join(CACHE_DIR, 'movies-box-office-dataset-cleaned.csv') 
+
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/box-office")
+def read_box_office_numbers():
+    df = pd.read_csv(dataset)
+    return df.to_dict("Rank")
